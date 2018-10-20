40c08345edccb1a7d1e0db1a8deb105e72ada8df
Horace Keung <horacekeung@hotmail.com>
2018-10-20 11:37:06 +0100

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,68 @@
+import path from 'path'
+import PurgecssPlugin from 'purgecss-webpack-plugin'
+import glob from 'glob-all'
+
+class TailwindExtractor {
+	static extract (content) {
+		return content.match(/[A-Za-z0-9-_:\/]+/g) || []
+	}
+}
+
+module.exports = {
+	mode: 'spa',
+	head: {
+		meta: [
+			{ name: 'twitter:card', content: 'summary_large_image' },
+			{ name: 'twitter:domain', content: 'https://horacekeung.github.io/why-vue/' },
+			{ property: 'og:image', name: 'twitter:image', itemprop: 'image primaryImageOfPage', content: 'https://horacekeung.github.io/why-vue/icon.png' },
+			{ property: 'og:image:width', content: '300' },
+			{ property: 'og:image:height', content: '300' },
+		],
+		script: process.env.DEPLOY_ENV === 'GH_PAGES' ? [{src: '/why-vue/gh-spa.js'}] : []
+	},
+	loading: false,
+	manifest: {
+		name: 'Why Vue',
+		short_name: 'Why Vue',
+		theme_color: '#41B883'
+	},
+	workbox: {
+		runtimeCaching: [{urlPattern: '/_nuxt/.*'}]
+	},
+	modules: ['@nuxtjs/pwa'],
+	build: {
+		extractCSS: true,
+		// Run ESLint on save
+		extend(config, { isDev, isClient }) {
+			if (isDev && isClient) {
+				config.module.rules.push({
+					enforce: 'pre',
+					test: /\.(js|vue)$/,
+					loader: 'eslint-loader',
+					exclude: /(node_modules)/
+				})
+			}
+			// Cleanup CSS with PurgeCSS
+			if (!isDev) {
+				config.plugins.push(
+					new PurgecssPlugin({
+						paths: glob.sync([
+							path.join(__dirname, './pages/**/*.vue'),
+							path.join(__dirname, './layouts/**/*.vue'),
+							path.join(__dirname, './components/**/*.vue')
+						]),
+						extractors: [{
+							extractor: TailwindExtractor,
+							extensions: ['vue']
+						}],
+						whitelist: ['html', 'body', 'nuxt-progress']
+					})
+				)
+			}
+		}
+	},
+	css: ['@/assets/css/global.css'],
+	router: {
+		base: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/why-vue/' : ''
+	}
+}
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>INDEX</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
